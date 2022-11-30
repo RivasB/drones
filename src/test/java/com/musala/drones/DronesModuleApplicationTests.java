6eb2006b0584c75db1fb0b93af1dd462a3a581a6
David Alejandro Rivas Bori <ucirivas@gmail.com>
2022-11-29 23:38:41 -0500

    (Fix) Minor changes in files

--- src/test/java/com/musala/drones/DronesModuleApplicationTests.java
+++ src/test/java/com/musala/drones/DronesModuleApplicationTests.java
@@ -1,10 +1,10 @@
-package com.lacarte.directory;
+package com.musala.drones;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DirectoryModuleApplicationTests {
+class DronesModuleApplicationTests {
 
 	@Test
 	void contextLoads() {
